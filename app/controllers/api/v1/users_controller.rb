d9a52a65e8cf0cece5d6476034a6039a9a9878e9
bogomilam <bogomilmihaylov@live.com>
2020-07-30 23:32:45 +0100

    fixed post user route

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,21 +1,21 @@
 class Api::V1::UsersController < ApplicationController
     #V @ 23.47 because of POST http://localhost:3000/api/v1/users 500 (Internal Server Error)
-
+    skip_before_action :verify_authenticity_token
+    
     def create
         user = User.new(user_params)
 
         if user.save
             render json: user
         else
-            render errors: user.errors.full_messages, status: :unauthorized
+            render json: errors: user.errors.full_messages, status: :unauthorized
         end
+    end
 
         private 
 
         def user_params
         params.require(:user).permit(:email, :password, :password_confirmation)
         end
-    end
-
 
 end
